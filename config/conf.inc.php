4d77c24102c53b212f46dc6588eefe8f3a649d11
Kevin Gautreau <kevin@kgaut.net>
2011-11-22 15:09:53 +0100

    ménage

--- config/conf.inc.php
+++ config/conf.inc.php
@@ -4,11 +4,15 @@ define('_fINCLUDE',true);
 define('DEBUG',false);
 $debug_var = array();
 
-$DB_HOST=''; 
-$DB_BASE=''; 
-$DB_USER=''; 
-$DB_PASS=''; 
+$DB_HOST=''; //serveur de base de données, localhost
+$DB_BASE=''; //nom de la base de données
+$DB_USER=''; //utilisateur se connectant à la base de données (généralement root)
+$DB_PASS=''; //mot de passe de connextion à la base de données
 
-require_once('functions/database.functions.php');
 
-$DBcon = DBconnexion($DB_HOST,$DB_BASE,$DB_USER,$DB_PASS);
+//on ne se connecte que si les infos sont rentrées
+if(trim($DB_HOST)!="") {
+    require_once('functions/database.functions.php');
+    $DBcon = DBconnexion($DB_HOST,$DB_BASE,$DB_USER,$DB_PASS);
+}
+
--- index.php
+++ index.php
@@ -1,15 +1,14 @@
-<?php
-	include('config/conf.inc.php');
-?>
+<?php include('config/conf.inc.php'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
 		<title>Mon Site</title>
 		<script type="text/javascript" src="js/scripts.js"></script>
-		<link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<link rel="stylesheet" type="text/css" href="template/css/styles.css" />
 	</head>
 	<body>
 		<div id="wrapper">
+            <p>ma pageee!</p>
 		</div>
 	</body>
 </html>
